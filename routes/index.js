4d3108253228124256bf904a825208af95202892
Jamie <40312490@live.napier.ac.uk>
2020-02-04 16:52:37 +0000

    Deposit and Withdraw working as expected

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,7 @@ let db = new sqlite3.Database(dbPath, sqlite3.OPEN_READWRITE, function(err) {
 });
 
 db.serialize(function() {
-	db.run("CREATE TABLE IF NOT EXISTS 'accounts' (email varchar(50) PRIMARY KEY NOT NULL, firstname varchar(50) NOT NULL, lastname varchar(50) NOT NULL, dateofbirth DATE NOT NULL, password varchar(255) NOT NULL, money REAL)", function(err) {
+	db.run("CREATE TABLE IF NOT EXISTS 'accounts' (email varchar(50) PRIMARY KEY NOT NULL, firstname varchar(50) NOT NULL, lastname varchar(50) NOT NULL, dateofbirth DATE NOT NULL, password varchar(255) NOT NULL, balance MONEY)", function(err) {
 		if(err) {
 			winston.error(err);
 		} else {
@@ -42,12 +42,12 @@ router.get('/', function(req, res, next) {
 
 router.get('/profile', function(req, res, next) {
 	if(req.session.loggedin) {
-		db.get("SELECT money FROM accounts WHERE email=?", req.session.email, function(error, result) {
+		db.get("SELECT balance FROM accounts WHERE email=?", req.session.email, function(error, result) {
 			if(error){
 				winston.error(error);
 				res.render('profile', { title: 'Error retrieving balance' } );
 			} else {
-				res.render('profile', { title: (req.session.name).charAt(0).toUpperCase() + req.session.name.substring(1), displaybalance: result.money } );
+				res.render('profile', { title: (req.session.name).charAt(0).toUpperCase() + req.session.name.substring(1), displaybalance: parseFloat(result.balance).toFixed(2) } );
 			}
 		});
 	} else {
@@ -57,7 +57,15 @@ router.get('/profile', function(req, res, next) {
 
 router.get('/profile/deposit', function(req, res, next) {
 	if(req.session.loggedin) {
-		res.render('deposit');
+		res.render('deposit', { title: 'Deposit' } );
+	} else {
+		res.redirect('/login');
+	}
+});
+
+router.get('/profile/withdraw', function(req, res, next) {
+	if(req.session.loggedin) {
+		res.render('withdraw', { title: 'Withdraw' } );
 	} else {
 		res.redirect('/login');
 	}
@@ -96,17 +104,39 @@ router.get('/logout', function(req, res, next) {
 });
 
 router.post('/profile/deposit', function(req, res, next) {
-	db.get('SELECT money FROM accounts WHERE email = ?', [(req.session.email).toLowerCase()], function(preerror, preresult) {
+	db.get('SELECT balance FROM accounts WHERE email = ?', [(req.session.email).toLowerCase()], function(preerror, preresult) {
 		if(preerror) {
 			winston.error(preerror);
 		} else {
-			db.run('UPDATE accounts SET money = ? Where email = ?', [preresult.money+req.body.depositAmount, (req.session.email).toLowerCase()], function(error, result) {
+			db.run('UPDATE accounts SET balance = ? Where email = ?', [parseFloat(preresult.balance)+parseFloat(req.body.depositAmount), (req.session.email).toLowerCase()], function(error) {
 				if(error) {
 					console.log(error);
 					winston.error(error);
 				} else {
-					winston.info('DEPOSIT EVENT: ' + req.session.email + 'AMOUNT: ' + req.body.depositAmount);
-					res.render('profile', { title: req.body.depositAmount + ' added to your account', displaybalance: result.money } );
+					winston.info('DEPOSIT EVENT: ' + req.session.email + ', AMOUNT: ' + req.body.depositAmount);
+					res.redirect('../profile');
+				}
+			});
+		}
+	});
+});
+
+router.post('/profile/withdraw', function(req, res, next) {
+	db.get('SELECT balance FROM accounts WHERE email = ?', [(req.session.email).toLowerCase()], function(preerror, preresult) {
+		if(preerror) {
+			winston.error(preerror);
+		} else if(req.body.withdrawAmount > preresult.balance){
+			res.render('withdraw', { funderror: "You don't have the funds to complete this action" } );
+		} else if(req.body.withdrawAmount < 0){
+			res.render('withdraw', { funderror: "Please enter a valid amount to withdraw" } );
+		} else {
+			db.run('UPDATE accounts SET balance = ? Where email = ?', [parseFloat(preresult.balance)-parseFloat(req.body.withdrawAmount), (req.session.email).toLowerCase()], function(error) {
+				if(error) {
+					console.log(error);
+					winston.error(error);
+				} else {
+					winston.info('WITHDRAW EVENT: ' + req.session.email + ', AMOUNT: ' + req.body.withdrawAmount);
+					res.redirect('../profile');
 				}
 			});
 		}
@@ -131,7 +161,7 @@ router.post('/creationauthenticate', function(req, res) {
 	}
 	
 	if(firstName && lastName && birthDate && email && password) {
-		db.run("INSERT INTO accounts (email, firstname, lastname, dateofbirth, password, money) VALUES (?,?,?,?,?,?)", email, firstName, lastName, birthDate, password, 100.69, function(error) {
+		db.run("INSERT INTO accounts (email, firstname, lastname, dateofbirth, password, balance) VALUES (?,?,?,?,?,?)", email, firstName, lastName, birthDate, password, 100.69, function(error) {
 			if(error) {
 				if(error.errno == 19) {
 					winston.error(error);
